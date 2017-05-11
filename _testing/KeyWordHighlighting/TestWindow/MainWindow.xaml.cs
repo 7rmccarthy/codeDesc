b49082ec20d1e0392de10d2fe46d917e407c905f
MaxiSchmid <MaxiSchmid100@gmx.at>
2017-05-11 14:31:57 +0200

    Added TabControl to KeyWordHighlighting

--- _testing/KeyWordHighlighting/TestWindow/MainWindow.xaml.cs
+++ _testing/KeyWordHighlighting/TestWindow/MainWindow.xaml.cs
@@ -1,14 +1,125 @@
 ﻿using System;
 using System.Windows;
 using System.IO;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
 
 namespace TestWindow
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
+        private List<TabItem> _tabItems;
+        private TabItem _tabAdd;
         public MainWindow()
         {
             InitializeComponent();
+            try
+            {
+                InitializeComponent();
+
+                // initialize tabItem array
+                _tabItems = new List<TabItem>();
+
+                // add a tabItem with + in header 
+                TabItem tabAdd = new TabItem();
+                tabAdd.Header = "+";
+
+                _tabItems.Add(tabAdd);
+
+                // add first tab
+                this.AddTabItem();
+
+                // bind tab control
+                tabDynamic.DataContext = _tabItems;
+
+                tabDynamic.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private TabItem AddTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+            tab.Header = string.Format("Tab {0}", count);
+            tab.Name = string.Format("tab{0}", count);
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+            tab.ContentTemplate = tabDynamic.FindResource("TabItem") as DataTemplate;
+            // add controls to tab item, this case I added just a textbox          
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+            return tab;
+        }
+        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = tabDynamic.SelectedItem as TabItem;
+
+            if (tab != null && tab.Header != null)
+            {
+                if (tab.Header.Equals("+"))
+                {
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    // add new tab
+                    TabItem newTab = this.AddTabItem();
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select newly added tab item
+                    tabDynamic.SelectedItem = newTab;
+                }
+                else
+                {
+                    // your code here...
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string tabName = (sender as Button).CommandParameter.ToString();
+
+            var item = tabDynamic.Items.Cast<TabItem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
+
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if (_tabItems.Count < 3)
+                {
+                    MessageBox.Show("Cannot remove last tab.");
+                }
+                else if (MessageBox.Show(string.Format("Are you sure you want to remove the tab '{0}'?", tab.Header.ToString()),
+                    "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    // get selected tab
+                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
+
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    _tabItems.Remove(tab);
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select previously selected tab. if that is removed then select first tab
+                    if (selectedTab == null || selectedTab.Equals(tab))
+                    {
+                        selectedTab = _tabItems[0];
+                    }
+                    tabDynamic.SelectedItem = selectedTab;
+                }
+            }
         }
 
         //  Open file dialog window button handler
@@ -36,7 +147,7 @@ namespace TestWindow
                             //  Add the read text to our scintilla object
                             Highlighting.Highlighting.AddText(sr.ReadToEnd());
                             //  Assign the windowsformshost child
-                            wfHost.Child = Highlighting.Highlighting.LanguageEditor;
+                            //wfHost.Child = Highlighting.Highlighting.LanguageEditor;
                             
                             //  Different method
                             //webBrowser.NavigateToString(new CodeColorizer().Colorize(sr.ReadToEnd(), Languages.CSharp));
@@ -53,6 +164,7 @@ namespace TestWindow
         private void wfHost_Loaded(object sender, RoutedEventArgs e)
         {
             Highlighting.Highlighting.Initialise();
+
         }
     }
 }
