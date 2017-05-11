d8ecc86f41d1ab3b457578c7edb4c04505360172
7rmccarthy <schellmann.raphael@me.com>
2017-05-11 10:38:52 +0200

    KeyWord highlighting library added

---
+++ _testing/KeyWordHighlighting/Hightlighting/Highlighting.cs
@@ -0,0 +1,139 @@
+﻿using System.Drawing;
+using SN = ScintillaNET;
+
+namespace Highlighting
+{
+    /*
+
+        ToDo:   Overload Initialise method to accept both custom and detected languages
+                Let user customise colour schemes
+                Customise autocompletion to accept custom variable names loaded into the form
+        Also:   maybe try to introduce code folding using #region
+    */
+    
+    //  Static class to be used in codeDesc for text visualisation
+    public static class Highlighting
+    {
+        //  Scintilla object
+        private static SN.Scintilla languageEditor;
+
+        //  Property
+        public static SN.Scintilla LanguageEditor
+        {
+            get { if (languageEditor != null) return languageEditor;
+                    else return new SN.Scintilla(); }
+            set { languageEditor = value; }
+        }
+
+        //  Initialisation method
+        public static void Initialise()
+        {
+            languageEditor = new SN.Scintilla();
+
+            //  Add event handler
+            languageEditor.CharAdded += charAdded;
+
+            loadConfiguration();
+            automaticCodeFolding();
+        }
+
+        //  Lets the user add text
+        public static void AddText(string source)
+        {
+            languageEditor.AddText(source);
+        }
+
+        //  Loads a (momentarily) custom c# configuration
+        private static void loadConfiguration()
+        {
+            //  Restore default values
+            languageEditor.StyleResetDefault();
+            languageEditor.Styles[SN.Style.Default].Font = "Consolas";
+            languageEditor.Styles[SN.Style.Default].Size = 10;
+            languageEditor.StyleClearAll();
+
+            //  Configure the style settings (Cpp standard language used for values we do not define)
+            languageEditor.Margins[0].Width = 40;
+            languageEditor.Styles[SN.Style.Cpp.Default].ForeColor = System.Drawing.Color.Silver;
+
+            //  Comments
+            languageEditor.Styles[SN.Style.Cpp.Comment].ForeColor = System.Drawing.Color.FromArgb(0, 128, 0);            //    Green
+            languageEditor.Styles[SN.Style.Cpp.CommentLine].ForeColor = System.Drawing.Color.FromArgb(0, 128, 0);        //    Green
+            languageEditor.Styles[SN.Style.Cpp.CommentLineDoc].ForeColor = System.Drawing.Color.FromArgb(128, 128, 128); //    Gray
+
+            //  Numbers
+            languageEditor.Styles[SN.Style.Cpp.Number].ForeColor = System.Drawing.Color.Olive;
+
+            //  Keywords
+            languageEditor.Styles[SN.Style.Cpp.Word].ForeColor = System.Drawing.Color.Blue;
+            languageEditor.Styles[SN.Style.Cpp.Word2].ForeColor = System.Drawing.Color.Turquoise;
+
+            //  Strings and characters
+            languageEditor.Styles[SN.Style.Cpp.String].ForeColor = System.Drawing.Color.FromArgb(163, 21, 21);           //    Red
+            languageEditor.Styles[SN.Style.Cpp.Character].ForeColor = System.Drawing.Color.FromArgb(163, 21, 21);        //    Red
+            languageEditor.Styles[SN.Style.Cpp.Verbatim].ForeColor = System.Drawing.Color.FromArgb(163, 21, 21);         //    Red
+            languageEditor.Styles[SN.Style.Cpp.StringEol].BackColor = System.Drawing.Color.Pink;
+
+            //  Misc
+            languageEditor.Styles[SN.Style.Cpp.Operator].ForeColor = System.Drawing.Color.Purple;
+            languageEditor.Styles[SN.Style.Cpp.Preprocessor].ForeColor = System.Drawing.Color.Maroon;
+            languageEditor.Styles[SN.Style.Cpp.GlobalClass].ForeColor = System.Drawing.Color.LightGreen;
+
+            //  Set the standard language to c++
+            languageEditor.Lexer = SN.Lexer.Cpp;
+
+            //  Set keywords
+            languageEditor.SetKeywords(0, "abstract as base break case catch checked continue default delegate do else event explicit extern false finally fixed for foreach goto if implicit in interface internal is lock namespace new null object operator out override params private protected public readonly ref return sealed sizeof stackalloc switch this throw true try typeof unchecked unsafe using virtual while bool byte char class const decimal double enum float int long sbyte short static string struct uint ulong ushort void");
+            languageEditor.SetKeywords(1, "Console Stream StreamReader OpenFileDialog"); // Own highlighted keywords
+        }
+
+        //  Automatically folds the code using braces
+        private static void automaticCodeFolding()
+        {
+            //  Instruct the lexer to calculate folding
+            languageEditor.SetProperty("fold", "1");
+            languageEditor.SetProperty("fold.compact", "1");
+
+            //  Configure a margin to display folding symbols
+            languageEditor.Margins[2].Type = SN.MarginType.Symbol;
+            languageEditor.Margins[2].Mask = SN.Marker.MaskFolders;
+            languageEditor.Margins[2].Sensitive = true;
+            languageEditor.Margins[2].Width = 20;
+
+            //  Set colours for all folding markers
+            for (int i = 25; i <= 31; i++)
+            {
+                languageEditor.Markers[i].SetForeColor(SystemColors.ControlLightLight);
+                languageEditor.Markers[i].SetBackColor(SystemColors.ControlDark);
+            }
+
+            //  Configure folding markers with respective symbols
+            languageEditor.Markers[SN.Marker.Folder].Symbol = SN.MarkerSymbol.BoxPlus;
+            languageEditor.Markers[SN.Marker.FolderOpen].Symbol = SN.MarkerSymbol.BoxMinus;
+            languageEditor.Markers[SN.Marker.FolderEnd].Symbol = SN.MarkerSymbol.BoxPlusConnected;
+            languageEditor.Markers[SN.Marker.FolderMidTail].Symbol = SN.MarkerSymbol.TCorner;
+            languageEditor.Markers[SN.Marker.FolderOpenMid].Symbol = SN.MarkerSymbol.BoxMinusConnected;
+            languageEditor.Markers[SN.Marker.FolderSub].Symbol = SN.MarkerSymbol.VLine;
+            languageEditor.Markers[SN.Marker.FolderTail].Symbol = SN.MarkerSymbol.LCorner;
+
+            //  Enable automatic folding
+            languageEditor.AutomaticFold = (SN.AutomaticFold.Show | SN.AutomaticFold.Click | SN.AutomaticFold.Change);
+        }
+
+        //  Simple word autocompletion method
+        private static void charAdded(object sender, SN.CharAddedEventArgs e)
+        {
+            //  Find the word start
+            var currentPos = languageEditor.CurrentPosition;
+            var wordStartPos = languageEditor.WordStartPosition(currentPos, true);
+
+            //  Display the autocompletion list
+            var lenEntered = currentPos - wordStartPos;
+            if (lenEntered > 0)
+            {
+                if (!languageEditor.AutoCActive)
+                    languageEditor.AutoCShow(lenEntered, "abstract as base break case catch checked continue default delegate do else event explicit extern false finally fixed for foreach goto if implicit in interface internal is lock namespace new null object operator out override params private protected public readonly ref return sealed sizeof stackalloc switch this throw true try typeof unchecked unsafe using virtual while void Console System");
+            }
+        }
+    }
+}
---
+++ _testing/KeyWordHighlighting/TestWindow/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+
+namespace TestWindow
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //  Open file dialog window button handler
+        private void insert_button_Click(object sender, RoutedEventArgs e)
+        {
+            //  Create a new openfiledialog
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+            //  Filter for *.cs files only
+            fileDialog.Filter = "c# files|*.cs";
+
+            //  Empty try-catch so the program does not crash upon unwanted file dialog actions
+            try
+            {
+                //  If the dialog has been opened successfully
+                if (fileDialog.ShowDialog() != null)
+                {
+                    Stream s;
+                    
+                    //  If the opened file is valid
+                    if ((s = fileDialog.OpenFile()) != null)
+                    {
+                        //  Read the file
+                        using (StreamReader sr = new StreamReader(s))
+                        {
+                            //  Add the read text to our scintilla object
+                            Highlighting.Highlighting.AddText(sr.ReadToEnd());
+                            //  Assign the windowsformshost child
+                            wfHost.Child = Highlighting.Highlighting.LanguageEditor;
+                            
+                            //  Different method
+                            //webBrowser.NavigateToString(new CodeColorizer().Colorize(sr.ReadToEnd(), Languages.CSharp));
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        //  Windowsformshost loading handler
+        private void wfHost_Loaded(object sender, RoutedEventArgs e)
+        {
+            Highlighting.Highlighting.Initialise();
+        }
+    }
+}
